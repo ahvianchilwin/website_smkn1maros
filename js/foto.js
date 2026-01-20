a27ad7965e93697e33f804104f10861b9db6a86b
ahvianchilwin <ahvianchilwin08@gmail.com>
2026-01-20 16:05:31 +0800

    redesign foto

--- js/foto.js
+++ js/foto.js
@@ -10,33 +10,3 @@ window.onclick = function(e) {
     }
   }
 }
-
-function openModal() {
-  document.getElementById("myModal").style.display = "block";
-}
-
-function closeModal() {
-  document.getElementById("myModal").style.display = "none";
-}
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSildes(n) {
-  showSlides(slideIndex += n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slides.slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
