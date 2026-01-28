164b1ed82a6021cc45bfaf744ecef23fff6d4dc3
ahvianchilwin <ahvianchilwin08@gmail.com>
2026-01-28 11:25:48 +0800

    re-disign home dan penambahan informasi futsal

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
+// Ambil elemen navbar dan hero section
+const navbar = document.querySelector(".navbar");
+const heroSection = document.querySelector(".hero");
 
-window.onclick = function(e) {
-  if (!e.target.matches('.dropbtn')) {
-  var myDropdown = document.getElementById("myDropdown");
-    if (myDropdown.classList.contains('show')) {
-      myDropdown.classList.remove('show');
+window.addEventListener('scroll', function() {
+    // 1. Cek berapa tinggi Hero Section saat ini
+    // Kita kurangi 100px agar transisinya mulai SESAAT sebelum benar-benar lewat (supaya lebih mulus)
+    const triggerHeight = heroSection.offsetHeight - 100;
+
+    // 2. Jika scroll sudah melewati batas tinggi hero...
+    if (window.scrollY > triggerHeight) {
+        navbar.classList.add("scrolled"); // Ubah jadi putih/biru
+    } else {
+        navbar.classList.remove("scrolled"); // Kembalikan jadi transparan
     }
-  }
-}
+});
