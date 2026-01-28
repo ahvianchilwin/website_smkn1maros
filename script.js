909ebb599306bd55f5ebc29ece90e4ccba66f70d
ahvianchilwin <ahvianchilwin08@gmail.com>
2026-01-28 16:28:03 +0800

    Memperbaiki JS

--- script.js
+++ script.js
@@ -1,16 +1,61 @@
-// Ambil elemen navbar dan hero section
+// Navbar
 const navbar = document.querySelector(".navbar");
 const heroSection = document.querySelector(".hero");
 
 window.addEventListener('scroll', function() {
-    // 1. Cek berapa tinggi Hero Section saat ini
-    // Kita kurangi 100px agar transisinya mulai SESAAT sebelum benar-benar lewat (supaya lebih mulus)
+    
     const triggerHeight = heroSection.offsetHeight - 100;
 
-    // 2. Jika scroll sudah melewati batas tinggi hero...
     if (window.scrollY > triggerHeight) {
-        navbar.classList.add("scrolled"); // Ubah jadi putih/biru
+        navbar.classList.add("scrolled"); 
     } else {
-        navbar.classList.remove("scrolled"); // Kembalikan jadi transparan
+        navbar.classList.remove("scrolled");
     }
 });
+
+// Foto
+const images =
+document.querySelectorAll('.row img');
+const lightbox =
+document.getElementById('lightbox');
+const lightboxImg =
+document.getElementById('lightbox-img');
+const caption =
+document.getElementById('data-caption');
+const closeBtn =
+document.querySelector('.close');
+
+images.forEach(img => {
+    img.addEventListener('click', () => {
+        lightbox.style.display = 'flex';
+        lightboxImg.src = img.src;
+        caption.textContent = img.dataset.caption;    
+    });
+});
+
+closeBtn.addEventListener('click', () => {
+    lightbox.style.display = 'none';    
+});
+
+lightbox.addEventListener('click', (e) => {
+    if (e.taget === lightbox) {
+        lightbox.style.display = 'none';
+    }
+});
+
+// Video
+var elements = document.getElementsByClassName("video-item");
+
+var i;
+
+function listView() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.width = "100%";
+  }
+}
+
+function gridView() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.width = "50%";
+  }
+}
